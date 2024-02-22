ad9f8987931950114fdc92e6e3006f87172cac63
Matias-07 <matias.ferrari.m@gmail.com>
2024-02-22 18:42:37 -0300

    update que funciona

--- Modulo_4/app.js
+++ Modulo_4/app.js
@@ -1,9 +1,11 @@
-// comando instalar cosas -------------> npm install dayjs   
+// comando instalar cosas -------------> npm install dayjs  / express
 
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
- res.send('¡Hola mundo!');
-});
-app.listen(3000, () =>
- console.log('Servidor corriendo')); 
+const express = require('express'); 
+
+const app = express(); 
+
+app.listen(3000, () => console.log('esto fue exitoso'))
+
+app.get('/', function(req, res){
+    res.send('Bienbenidos al sitio')
+});   
